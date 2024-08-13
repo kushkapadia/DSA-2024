13552fb837dba3ede15091fe2d8893be5afc9bcd
Kush Kapadia <kush.salesbrahma@gmail.com>
2024-08-14 00:07:59 +0530

    Added 40-Combination_Sum_II.java

---
+++ Leetcode/40-Combination_Sum_II.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public void findCombinations(int ind, int[] candidates, int target, List<List<Integer>> ans, List<Integer> ds) {
+
+        if (target == 0) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for (int i = ind; i < candidates.length; i++) {
+            if (i > ind && candidates[i] == candidates[i - 1])
+                continue;
+            if (candidates[i] > target)
+                break;
+            ds.add(candidates[i]);
+            findCombinations(i + 1, candidates, target - candidates[i] , ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList();
+        Arrays.sort(candidates);
+        findCombinations(0, candidates, target, ans, new ArrayList());
+        return ans;
+    }
+
+}
