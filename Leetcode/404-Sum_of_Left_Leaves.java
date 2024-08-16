8110581c7fd112cb3ec236bfa399b5bba690c15d
Kush Kapadia <kush.salesbrahma@gmail.com>
2024-08-16 21:48:21 +0530

    Added 404-Sum_of_Left_Leaves.java

--- Leetcode/404-Sum_of_Left_Leaves.java
+++ Leetcode/404-Sum_of_Left_Leaves.java
@@ -15,29 +15,41 @@
  */
 class Solution {
 
-public int sum(TreeNode root, TreeNode parent){
+// public int sum(TreeNode root, TreeNode parent){
+//     if(root == null){
+//         return 0;
+//     }
+//     if(parent != null){
+// if(root.left == null && root.right == null && root == parent.left){
+// return root.val;
+// }
+//  int x = sum(root.left, root);
+//     int y = sum(root.right, root);
+//     return x+y;
+//     }
+//     else{
+//         int x = sum(root.left, root);
+//     int y = sum(root.right, root);
+//     return x+y;
+//     }
+    
+// }
+
+public int sum (TreeNode root, boolean left){
     if(root == null){
         return 0;
     }
-    if(parent != null){
-if(root.left == null && root.right == null && root == parent.left){
-return root.val;
-}
- int x = sum(root.left, root);
-    int y = sum(root.right, root);
-    return x+y;
+    if(left==true && root.left == null && root.right == null){
+        return root.val;
     }
-    else{
-        int x = sum(root.left, root);
-    int y = sum(root.right, root);
+    int x = sum(root.left, true);
+    int y = sum(root.right, false);
     return x+y;
-    }
-    
 }
 
     public int sumOfLeftLeaves(TreeNode root) {
         
-        int sum = sum(root, null);
+        int sum = sum(root, false);
         return sum;
 
     }
