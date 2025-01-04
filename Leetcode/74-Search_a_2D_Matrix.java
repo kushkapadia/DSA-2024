3357a42ff3d187ae70b7f3dfed45d4dc558b0327
Kush Kapadia <72224801+kushkapadia@users.noreply.github.com>
2025-01-04 10:25:40 +0530

    Update 74-Search_a_2D_Matrix.java

--- Leetcode/74-Search_a_2D_Matrix.java
+++ Leetcode/74-Search_a_2D_Matrix.java
@@ -3,20 +3,20 @@ class Solution {
         int n = matrix.length; //no.of rows
         int m = matrix[0].length; //no.of columns
 
-        int low = 0;
-        int high = m*n -1;
+        int low = 0; 
+        int high = m*n -1; //high == last index of matrix
 
         while(low<=high){
             int mid = (low + high) / 2;
-            int rowIndex = mid/m;
-            int columnIndex = mid%m;
+            int rowIndex = mid/m; //to convert 1d array index into 2d array row index;
+            int columnIndex = mid%m; //to convert 1d array index into 2d array column index;
 
             if( matrix[rowIndex][columnIndex] == target ){
-                return true;
+                return true; //if mid elemnt is target then return true;
             } else if(matrix[rowIndex][columnIndex] < target){
-                low = mid + 1;
+                low = mid + 1; //if m,id element is less then target, then search to the right.
             } else{
-                high = mid - 1;
+                high = mid - 1; //else search to the left.
             }
         }
         return false;
