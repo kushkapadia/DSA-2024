338605c6058270d25c00a983dd7df1cd0cbd864a
Kush Kapadia <kush.salesbrahma@gmail.com>
2025-02-01 17:40:58 +0530

    Added 3151-Special_Array_I.java

--- Leetcode/3151-Special_Array_I.java
+++ Leetcode/3151-Special_Array_I.java
@@ -1,36 +1,46 @@
+// class Solution {
+//     public boolean checkparity(int num1, int num2){
+//         boolean r1 =false;
+//         boolean r2 =false;
+//         if(num1 %2 == 0){
+//             r1 = true;
+//         }
+//         if(num2%2==0){
+//             r2 =true;
+//         }
+
+//         if(r1 == r2){
+//             return false;
+//         } else{
+//             return true;
+//         }
+//     }
+//     public boolean isArraySpecial(int[] nums) {
+//         if(nums.length == 1 ){
+//             return true;
+//         }
+
+//         for(int i = 1; i<nums.length; i++){
+//             if(!checkparity(nums[i-1], nums[i])){
+//              return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// }
+
+
 class Solution {
-    public boolean checkparity(int num1, int num2){
-        boolean r1 =false;
-        boolean r2 =false;
-        if(num1 %2 == 0){
-            r1 = true;
-        }
-        if(num2%2==0){
-            r2 =true;
-        }
-
-        if(r1 == r2){
-            return false;
-        } else{
-            return true;
-        }
-    }
     public boolean isArraySpecial(int[] nums) {
-        if(nums.length == 1 ){
-            return true;
-        }
+        if (nums.length == 1) return true;
 
-        for(int i = 1; i<nums.length; i++){
-            if(!checkparity(nums[i-1], nums[i])){
-             return false;
-            }
+        for (int i = 1; i < nums.length; i++) {
+            if ((nums[i - 1] % 2) == (nums[i] % 2)) return false;
         }
-
         return true;
     }
 }
-
-
 
 //214
 // i =1 && i == i-1
